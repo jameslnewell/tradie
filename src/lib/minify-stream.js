edf8914a07078954b830446e2a7aefb319a14f81
eastwood <clintryan3@gmail.com>
2016-03-27 03:24:35 +1100

    Fixed regression in minify stream, soon to add tests
    Fixed another regression
    
    Added test around the autoprefixer
    
    Improved readability

--- src/lib/minify-stream.js
+++ src/lib/minify-stream.js
@@ -5,7 +5,7 @@ module.exports = function(options) {
   let data = '';
 
   return through(
-    chunk, enc, callback => {
+    function(chunk, enc, callback) {
       data += chunk.toString();
       callback();
     },
@@ -14,7 +14,7 @@ module.exports = function(options) {
       const cleaner = new clean(options);
       cleaner.minify(data, (err, result) => {
         if (err) return callback(err);
-        push(result.styles);
+        this.push(result.styles);
         callback();
       });
     }
