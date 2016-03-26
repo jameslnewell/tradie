a9bf9dff0ba80fb735bf013610de66ba435ad797
James Newell <james@digitaledgeit.com.au>
2016-03-26 21:44:16 +1100

    got watching working

--- src/lib/scripts/test.js
+++ src/lib/scripts/test.js
@@ -10,7 +10,6 @@ import sourceMapper from 'source-mapper';
 import readMochaOptions from '../readMochaOptions';
 
 const mochaSetup = `
-//'use strict';
 
 const Mocha = require('mocha');
 Mocha.reporters.Base.window.width = ${process.stdout.columns || 80};
@@ -27,7 +26,6 @@ setTimeout(() => {
     process.exit(errors ? 1 : 0);
   });
 }, 1);
-
 `;
 
 class MochaSetupPlugin {
@@ -84,76 +82,10 @@ export default function({args, config, emitter}) {
 
   };
 
-  //bundle the test files
-  const createTestBundle = () => {
-    return new Promise((resolve, reject) => {
-
-      findTestFiles()
-        .then(files => {
-
-          const entries = [].concat(
-            mochaRequire,
-            files
-          ).map(entry => './'+entry); //make the entries local files
-
-          const webpackConfig = {
-            target: 'node',
-            devtool: 'inline-source-map',
-            context: path.resolve(src),
-            entry: entries,
-            output: {
-              path: path.resolve(dest),
-              filename: 'bundle.js'
-            },
-            resolve: {
-              extensions: [''].concat(extensions)
-            },
-            module: {
-              loaders: [
-                transforms.map(name => ({
-                  exclude: /(node_modules)/,
-                  loader: 'babel-loader'
-                }))
-              ],
-            },
-            plugins: [
-              new MochaSetupPlugin()
-            ]
-          };
-
-          console.log(webpackConfig.loaders);
-
-          const fs = new MemoryFS();
-          const compiler = webpack(webpackConfig);
-
-          compiler.outputFileSystem = fs;
-
-          compiler.run((err, stats) => {
-            if (err) return reject(err);
-
-            const jsonStats = stats.toJson();
-
-            if (jsonStats.errors.length > 0) {
-              console.log(jsonStats.errors.join('\n'));
-              reject(jsonStats.errors);
-            } else {
-              //TODO: what if webpack splits it in more than one chunk?)
-              resolve(fs.readFileSync(path.join(path.resolve(dest), 'bundle.js')).toString());
-            }
-
-          });
-
-        })
-        .catch(reject)
-      ;
-
-    });
-  };
-
   //run the test bundle on nodejs
   const runTestBundle = (bundle) => {
     return new Promise((resolve, reject) => {
-console.log('running');
+
       //run in a sub-directory of `tradie` so that the `mocha` package is found
       //FIXME: should probably be run in the `.tradierc` directory
       const node = spawn('node', {cwd: __dirname});
@@ -186,14 +118,102 @@ console.log('running');
     });
   };
 
+  //bundle the test files
+  const createTestBundle = () => {
+    return new Promise((resolve, reject) => {
+
+      findTestFiles()
+        .then(files => {
+
+          const entries = [].concat(
+            mochaRequire,
+            files
+          ).map(entry => './'+entry); //make the entries local files - TODO: should we force tradie users to do this themselves?
+
+          const webpackConfig = {
+            target: 'node',
+            devtool: 'inline-source-map',
+            context: path.resolve(src),
+            entry: entries,
+            output: {
+              path: path.resolve(dest),
+              filename: 'bundle.js'
+            },
+            resolve: {
+              extensions: [''].concat(extensions)
+            },
+            module: {
+              loaders: [
+                transforms.map(loader => ({
+                  exclude: /(node_modules)/,
+                  loader
+                }))
+              ]
+            },
+            plugins: [
+              new MochaSetupPlugin()
+            ]
+          };
+
+          const fs = new MemoryFS();
+          const compiler = webpack(webpackConfig);
+
+          compiler.outputFileSystem = fs;
+
+          if (watch) {
+
+            const watcher = compiler.watch({}, (err, stats) => {
+              if (err) return; //TODO: log error?
+
+              const jsonStats = stats.toJson();
+
+              if (jsonStats.errors.length > 0) {
+                console.log(jsonStats.errors.join('\n'));
+                reject(jsonStats.errors);//FIXME:
+              } else {
+
+                //TODO: what if webpack splits it in more than one chunk?)
+                runTestBundle(fs.readFileSync(path.join(path.resolve(dest), 'bundle.js')).toString())
+              }
+
+            });
+
+            //stop watching and exit when the user presses CTL-C
+            process.on('SIGINT', () => {
+              watcher.close(() => resolve(0));
+            });
+
+
+          } else {
+
+            compiler.run((err, stats) => {
+              if (err) return reject(err);
+
+              const jsonStats = stats.toJson();
+
+              if (jsonStats.errors.length > 0) {
+                console.log(jsonStats.errors.join('\n'));
+                reject(jsonStats.errors);//FIXME:
+              } else {
+                //TODO: what if webpack splits it in more than one chunk?)
+                resolve(runTestBundle(fs.readFileSync(path.join(path.resolve(dest), 'bundle.js')).toString()));
+              }
+
+            });
+
+          }
+
+        })
+        .catch(reject)
+      ;
+
+    });
+  };
+
   return new Promise((resolve, reject) => {
 
-    emitter.emit('scripts.testing.started');
     createTestBundle()
-      .then(runTestBundle)
       .then(exitCode => {
-
-        emitter.emit('scripts.testing.finished');
 
         //if we're not watching then we're done
         if (!watch) {
