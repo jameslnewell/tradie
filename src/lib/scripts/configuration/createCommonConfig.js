d58ec1dae6e0b61cdb58fab7316cd67dbae9b720
James Newell <james@digitaledgeit.com.au>
2016-04-16 03:27:14 +1000

    limit loaders to script extensions

--- src/lib/scripts/configuration/createCommonConfig.js
+++ src/lib/scripts/configuration/createCommonConfig.js
@@ -4,15 +4,17 @@ export default function createCommonConfig(options) {
 
   const allLoaders = []
 
-  //TODO: we only apply loaders to project specific code, in the future, we'll probably want to provide a way to override this convention e.g. string vs object
+  //TODO: we only apply loaders to project specific script files, in the future, we'll probably want to provide a way to override this convention e.g. using an object instead of a string
     .concat(
       loaders.map(loader => ({
+        test:  new RegExp(extensions.join('$|').replace('.', '\\.') + '$'),
         exclude: /(node_modules)/,
         loader
       }))
     )
 
     //browserify loads JSON files like NodeJS does... emulate that for compatibility
+    //note: last loader is evaluated first http://stackoverflow.com/questions/32234329/what-is-the-loader-order-for-webpack
     .concat({
       test: /\.json$/,
       loader: 'json'
