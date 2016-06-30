c921c46f2fd0d3bcc0711837b2417a361c55f1b7
James Newell <j.newell@nib.com.au>
2016-06-30 15:36:34 +1000

    remove es6 to fix acceptance tests

--- test/lint.js
+++ test/lint.js
@@ -4,7 +4,7 @@ const spawn = require('child_process').spawn;
 function run(args, options) {
   return new Promise((resolve, reject) => {
 
-    const proc = spawn('node', [path.resolve('./dist/bin/tradie.js'), ...args], options);
+    const proc = spawn('node', [].concat(path.resolve('./dist/bin/tradie.js'), args), options);
 
     if (options.log) {
       proc.stdout.pipe(process.stdout);
