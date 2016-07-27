d0cc348f457b834cb2f7eec6b64f33f64c831335
James Newell <james@digitaledgeit.com.au>
2016-07-28 09:58:58 +1000

    added a test to ensure lint warnings don't give an error exit code

--- test/lint.js
+++ test/lint.js
@@ -4,13 +4,19 @@ import exec from './util/exec';
 describe('tradie lint', function() {
   this.timeout(5000);
 
-  it('should exit with 0 when there are no linting errors', () => {
+  it('should exit with 0 when there are no lint warnings and no lint errors', () => {
     return exec(['lint'], {cwd: path.resolve('./test/fixture/lint-ok')})
       .then(code => expect(code).to.be.equal(0))
     ;
   });
 
-  it('should exit with 1 when there are linting errors', () => {
+  it('should exit with 0 when there are lint warnings and no lint errors', () => {
+    return exec(['lint'], {cwd: path.resolve('./test/fixture/lint-warn')})
+      .then(code => expect(code).to.be.equal(0))
+      ;
+  });
+
+  it('should exit with 1 when there are lint errors', () => {
     return exec(['lint'], {cwd: path.resolve('./test/fixture/lint-err')})
       .then(code => expect(code).to.be.equal(1))
     ;
